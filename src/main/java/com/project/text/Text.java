e5e48798a9ccc0456da2ce3f2ffeb0994f2f7907
Erick <127678881+Erillope@users.noreply.github.com>
2023-07-19 20:33:31 -0500

    ;)

--- src/main/java/com/project/text/Text.java
+++ src/main/java/com/project/text/Text.java
@@ -10,6 +10,7 @@ public class Text {
         
         image.generarTexto("imagen_text.txt");
         //Las imágenes se ingresan en la carpeta de imágenes y los textos se guardan en la carpeta txt
+        //Al abrir el block de notas, configurar el tamaño de la fuente a 1 para que se visualize bien.
 
     }
 }
